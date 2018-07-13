153e2571996dd264b3029408885fe833a4f84a4c
emrah karaman <ekaraman89@hotmail.com>
2018-07-13 13:44:04 +0200

    initial commit

---
+++ TopluYazdir/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace TopluYazdir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.lstBoxFiles = new System.Windows.Forms.ListBox();
+            this.chkListFirma = new System.Windows.Forms.CheckedListBox();
+            this.chkListDonem = new System.Windows.Forms.CheckedListBox();
+            this.chkListTur = new System.Windows.Forms.CheckedListBox();
+            this.rdBtnAll = new System.Windows.Forms.RadioButton();
+            this.rdBtnBeyanname = new System.Windows.Forms.RadioButton();
+            this.rdBtnTahakkuk = new System.Windows.Forms.RadioButton();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(126, 23);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Dosya Yolu Ayarla";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // lstBoxFiles
+            // 
+            this.lstBoxFiles.FormattingEnabled = true;
+            this.lstBoxFiles.Location = new System.Drawing.Point(12, 232);
+            this.lstBoxFiles.Name = "lstBoxFiles";
+            this.lstBoxFiles.Size = new System.Drawing.Size(767, 212);
+            this.lstBoxFiles.TabIndex = 1;
+            // 
+            // chkListFirma
+            // 
+            this.chkListFirma.CheckOnClick = true;
+            this.chkListFirma.FormattingEnabled = true;
+            this.chkListFirma.Location = new System.Drawing.Point(12, 46);
+            this.chkListFirma.Name = "chkListFirma";
+            this.chkListFirma.Size = new System.Drawing.Size(290, 169);
+            this.chkListFirma.TabIndex = 2;
+            this.chkListFirma.SelectedIndexChanged += new System.EventHandler(this.chkListFirma_SelectedIndexChanged);
+            // 
+            // chkListDonem
+            // 
+            this.chkListDonem.CheckOnClick = true;
+            this.chkListDonem.FormattingEnabled = true;
+            this.chkListDonem.Location = new System.Drawing.Point(308, 47);
+            this.chkListDonem.Name = "chkListDonem";
+            this.chkListDonem.Size = new System.Drawing.Size(144, 169);
+            this.chkListDonem.TabIndex = 3;
+            this.chkListDonem.SelectedIndexChanged += new System.EventHandler(this.chkListDonem_SelectedIndexChanged);
+            // 
+            // chkListTur
+            // 
+            this.chkListTur.CheckOnClick = true;
+            this.chkListTur.FormattingEnabled = true;
+            this.chkListTur.Location = new System.Drawing.Point(469, 47);
+            this.chkListTur.Name = "chkListTur";
+            this.chkListTur.Size = new System.Drawing.Size(144, 169);
+            this.chkListTur.TabIndex = 4;
+            this.chkListTur.SelectedIndexChanged += new System.EventHandler(this.chkListTur_SelectedIndexChanged);
+            // 
+            // rdBtnAll
+            // 
+            this.rdBtnAll.AutoSize = true;
+            this.rdBtnAll.Checked = true;
+            this.rdBtnAll.Location = new System.Drawing.Point(649, 62);
+            this.rdBtnAll.Name = "rdBtnAll";
+            this.rdBtnAll.Size = new System.Drawing.Size(52, 17);
+            this.rdBtnAll.TabIndex = 5;
+            this.rdBtnAll.TabStop = true;
+            this.rdBtnAll.Text = "Hepsi";
+            this.rdBtnAll.UseVisualStyleBackColor = true;
+            this.rdBtnAll.CheckedChanged += new System.EventHandler(this.rdBtnAll_CheckedChanged);
+            // 
+            // rdBtnBeyanname
+            // 
+            this.rdBtnBeyanname.AutoSize = true;
+            this.rdBtnBeyanname.Location = new System.Drawing.Point(649, 85);
+            this.rdBtnBeyanname.Name = "rdBtnBeyanname";
+            this.rdBtnBeyanname.Size = new System.Drawing.Size(81, 17);
+            this.rdBtnBeyanname.TabIndex = 6;
+            this.rdBtnBeyanname.Text = "Beyanname";
+            this.rdBtnBeyanname.UseVisualStyleBackColor = true;
+            this.rdBtnBeyanname.CheckedChanged += new System.EventHandler(this.rdBtnBeyanname_CheckedChanged);
+            // 
+            // rdBtnTahakkuk
+            // 
+            this.rdBtnTahakkuk.AutoSize = true;
+            this.rdBtnTahakkuk.Location = new System.Drawing.Point(649, 109);
+            this.rdBtnTahakkuk.Name = "rdBtnTahakkuk";
+            this.rdBtnTahakkuk.Size = new System.Drawing.Size(74, 17);
+            this.rdBtnTahakkuk.TabIndex = 7;
+            this.rdBtnTahakkuk.Text = "Tahakkuk";
+            this.rdBtnTahakkuk.UseVisualStyleBackColor = true;
+            this.rdBtnTahakkuk.CheckedChanged += new System.EventHandler(this.rdBtnTahakkuk_CheckedChanged);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(649, 161);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(130, 23);
+            this.btnPrint.TabIndex = 8;
+            this.btnPrint.Text = "Yazıcıya Gönder";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.rdBtnTahakkuk);
+            this.Controls.Add(this.rdBtnBeyanname);
+            this.Controls.Add(this.rdBtnAll);
+            this.Controls.Add(this.chkListTur);
+            this.Controls.Add(this.chkListDonem);
+            this.Controls.Add(this.chkListFirma);
+            this.Controls.Add(this.lstBoxFiles);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.ListBox lstBoxFiles;
+        private System.Windows.Forms.CheckedListBox chkListFirma;
+        private System.Windows.Forms.CheckedListBox chkListDonem;
+        private System.Windows.Forms.CheckedListBox chkListTur;
+        private System.Windows.Forms.RadioButton rdBtnAll;
+        private System.Windows.Forms.RadioButton rdBtnBeyanname;
+        private System.Windows.Forms.RadioButton rdBtnTahakkuk;
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
+
---
+++ TopluYazdir/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TopluYazdir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private class Dosya
+        {
+            public string Firma { get; set; }
+            public string Donem { get; set; }
+            public string Tur { get; set; }
+            public string Cins { get; set; }
+            public string Yol { get; set; }
+
+            public Dosya(FileInfo info)
+            {
+                Firma = info.Directory.Parent.Name;
+                Donem = info.Directory.Name;
+                string[] tmp = info.Name.Split('_');
+                Tur = tmp[0];
+                Cins = tmp[1];
+                Yol = info.FullName;
+            }
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            GetAllFiles();
+            var ts = lst.GroupBy(x => x.Firma).Select(y => y.First().Firma).ToArray();
+
+            chkListFirma.Items.AddRange(ts);
+        }
+
+        List<Dosya> lst = new List<Dosya>();
+        List<Dosya> tmp = new List<Dosya>();
+
+        List<String> Firmalar = new List<string>();
+        List<String> Donemler = new List<string>();
+        List<String> Tur = new List<string>();
+
+        private void GetAllFiles()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+            string[] allfiles = Directory.GetFiles(fbd.SelectedPath, "*.*", SearchOption.AllDirectories);
+
+            foreach (var file in allfiles)
+            {
+                FileInfo info = new FileInfo(file);
+                Dosya dosya = new Dosya(info);
+                lst.Add(dosya);
+            }
+        }
+
+        private List<Dosya> Query(Func<Dosya, bool> condition)
+        {
+            return lst.Where(condition).ToList();
+        }
+
+        private void chkListFirma_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Firmalar.Clear();
+            foreach (string item in chkListFirma.CheckedItems)
+            {
+                Firmalar.Add(item);
+            }
+
+            lstBoxFiles.Items.Clear();
+
+            tmp = Query(x => Firmalar.Contains(x.Firma));
+            lstBoxFiles.Items.AddRange(tmp.Select(x => x.Yol).ToArray());
+
+            var ts = tmp.GroupBy(x => x.Donem).Select(y => y.First().Donem).ToArray();
+            chkListDonem.Items.Clear();
+            chkListDonem.Items.AddRange(ts);
+        }
+
+        private void chkListDonem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Donemler.Clear();
+            foreach (string item in chkListDonem.CheckedItems)
+            {
+                Donemler.Add(item);
+            }
+
+            lstBoxFiles.Items.Clear();
+
+
+            var ts = tmp.Where(x => Donemler.Contains(x.Donem)).ToList();
+
+            chkListTur.Items.Clear();
+            chkListTur.Items.AddRange(ts.GroupBy(x => x.Tur).Select(y => y.First().Tur).ToArray());
+
+            // var lstforlstbox = tmp.Where(x => Donemler.Contains(x.Donem)).ToList().GroupBy(x => new { x.Tur, x.Cins }).ToList();
+            // listBox1.Items.AddRange(lstforlstbox.Select(x => x.First().Yol).ToArray());
+            // listBox1.Items.AddRange(ts.GroupBy(x => x.Tur).Select(x => x.First().Yol).ToArray());
+            lstBoxFiles.Items.AddRange(ts.Select(x => x.Yol).ToArray());
+
+
+        }
+
+        private void chkListTur_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Tur.Clear();
+            foreach (string item in chkListTur.CheckedItems)
+            {
+                Tur.Add(item);
+            }
+            lstBoxFiles.Items.Clear();
+            var ts = tmp.Where(x => Donemler.Contains(x.Donem) && Tur.Contains(x.Tur)).ToList();
+
+            lstBoxFiles.Items.AddRange(ts.Select(x => x.Yol).ToArray());
+
+        }
+
+        private void rdBtnAll_CheckedChanged(object sender, EventArgs e)
+        {
+            var ts = tmp.Where(x => Donemler.Contains(x.Donem) && Tur.Contains(x.Tur)).ToList();
+            lstBoxFiles.Items.Clear();
+            lstBoxFiles.Items.AddRange(ts.Select(x => x.Yol).ToArray());
+        }
+
+        private void rdBtnBeyanname_CheckedChanged(object sender, EventArgs e)
+        {
+            var ts = tmp.Where(x => Donemler.Contains(x.Donem) && Tur.Contains(x.Tur) && x.Cins.ToLower().Contains("beyanname")).ToList();
+            lstBoxFiles.Items.Clear();
+
+            lstBoxFiles.Items.AddRange(ts.Select(x => x.Yol).ToArray());
+
+        }
+
+        private void rdBtnTahakkuk_CheckedChanged(object sender, EventArgs e)
+        {
+            var ts = tmp.Where(x => Donemler.Contains(x.Donem) && Tur.Contains(x.Tur) && x.Cins.ToLower().Contains("tahakkuk")).ToList();
+            lstBoxFiles.Items.Clear();
+
+            lstBoxFiles.Items.AddRange(ts.Select(x => x.Yol).ToArray());
+
+        }
+
+        private void SendToPrinter(string file)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.Verb = "print";
+            info.FileName = file;
+            info.CreateNoWindow = true;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process p = new Process();
+            p.StartInfo = info;
+            p.Start();
+
+            p.WaitForInputIdle();
+            System.Threading.Thread.Sleep(3000);
+            if (false == p.CloseMainWindow())
+                p.Kill();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            if (lstBoxFiles.Items.Count > 0)
+            {
+                if (MessageBox.Show(lstBoxFiles.Items.Count + " adet dosya yazıcıya gönderilecek. Onaylıyor musunuz ?", "Yazdırma Onayı", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    foreach (var item in lstBoxFiles.Items)
+                    {
+                        SendToPrinter(item.ToString());
+                    }
+            }
+            else
+            {
+                MessageBox.Show("Liste Boş");
+            }
+        }
+
+    }
+}
