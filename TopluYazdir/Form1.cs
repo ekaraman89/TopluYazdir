9361a5954c88c64ff62fa28677f4ad7406a996c2
ekaraman89 <ekaraman89@hotmail.com>
2018-07-16 17:24:55 +0300

    Kullanılmayan kütüphaneler silindi

--- TopluYazdir/Form1.cs
+++ TopluYazdir/Form1.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Diagnostics;
-using System.Drawing.Printing;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
